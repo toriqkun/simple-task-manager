aa1b00e7cf9131533e5881541b65c5294dfca5b1
Toriq Abdul Rosyid <toriqkun25@gmail.com>
2026-03-13 15:44:39 +0700

    feat: setup prisma schema and database types

---
+++ backend/dist/prisma.config.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const node_process_1 = __importDefault(require("node:process"));
+const config_1 = require("prisma/config");
+exports.default = (0, config_1.defineConfig)({
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    datasource: {
+        url: node_process_1.default.env["DATABASE_URL"],
+    },
+});
---
+++ backend/src/dto/task.dto.ts
@@ -0,0 +1,5 @@
+export interface CreateTaskDTO {
+  title: string
+  description?: string
+  completed?: boolean
+}
