24c4a57a2b1572aa729d8edeedd95a446d190d30
Toriq Abdul Rosyid <toriqkun25@gmail.com>
2026-03-13 17:33:43 +0700

    feat: rename files and migration database

---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import userController from '../controllers/userController';
+
+const router = Router();
+
+router.post('/', userController.register);
+router.get('/', userController.getAll);
+router.get('/:id', userController.getById);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+
+export default router;
