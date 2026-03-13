554c7b1e5e5d23e9ac597a4c06073415d46ce7ae
Toriq Abdul Rosyid <toriqkun25@gmail.com>
2026-03-13 15:47:35 +0700

    feat: implement user repository

---
+++ backend/dist/src/repositories/user.repository.js
@@ -0,0 +1,40 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserRepository = void 0;
+const prisma_1 = __importDefault(require("../config/prisma"));
+class UserRepository {
+    async create(data) {
+        return prisma_1.default.user.create({
+            data,
+        });
+    }
+    async findAll() {
+        return prisma_1.default.user.findMany();
+    }
+    async findById(id) {
+        return prisma_1.default.user.findUnique({
+            where: { id },
+        });
+    }
+    async findByEmail(email) {
+        return prisma_1.default.user.findUnique({
+            where: { email },
+        });
+    }
+    async update(id, data) {
+        return prisma_1.default.user.update({
+            where: { id },
+            data,
+        });
+    }
+    async delete(id) {
+        return prisma_1.default.user.delete({
+            where: { id },
+        });
+    }
+}
+exports.UserRepository = UserRepository;
+exports.default = new UserRepository();
