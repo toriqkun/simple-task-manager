2d6c8175486099242ca85eab2c9cd82a338b3d49
Toriq Abdul Rosyid <toriqkun25@gmail.com>
2026-03-14 21:51:38 +0700

    fix: move types to dependencies to fix production build

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,10 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
-dotenv.config();
-
 import userRoutes from './routes/userRoutes';
 import taskRoutes from './routes/taskRoutes';
+
+dotenv.config();
 
 const app: Application = express();
 
@@ -20,9 +19,5 @@ app.use(cookieParser());
 
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
-
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to Task Manager API' });
-});
 
 export default app;
